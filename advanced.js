cba40e509c8043a0fc5fa3c36ef2786796b3e244
Surya Prabhat Saurabh <154997765+suryaprabhat-saurabh@users.noreply.github.com>
2026-02-26 18:15:34 +0530

    Save to DB

--- advanced.js
+++ advanced.js
@@ -1,18 +1,35 @@
-let box=document.querySelector(".box")
+// let box=document.querySelector(".box")
 
-function changeColor(color,delay,nextColor){
-    setTimeout(() => {
-        if(nextColor)nextColor()
-            box.style.backgroundColor=color
-    },delay);
-}
+// function changeColor(color,delay,nextColor){
+//     setTimeout(() => {
+//         if(nextColor)nextColor()
+//             box.style.backgroundColor=color
+//     },delay);
+// }
 
-changeColor("blue",2000, ()=>{
-    changeColor("red",2000, ()=>{
-        changeColor("green",2000,()=>{
-            changeColor("yellow",2000,()=>{
-                changeColor("orange",2000)
-            })
-        })
-    })
-})
+// changeColor("blue",2000, ()=>{
+//     changeColor("red",2000, ()=>{
+//         changeColor("green",2000,()=>{
+//             changeColor("yellow",2000,()=>{
+//                 changeColor("orange",2000)
+//             })
+//         })
+//     })
+// })
+
+// function saveToDb (data,success,failure){
+//     let internetSpeed=Math.floor(Math.random()*10)+1
+
+//     if(internetSpeed>4){
+//         success(internetSpeed)
+//     }
+//         else{
+//             failure(internetSpeed)
+//     }
+// }
+
+// saveToDb("Vishnu",(internetSpeed)=>{
+//     console.log("Data 1 Saved :" , internetSpeed)
+// },(internetSpeed)=>{
+//     console.log("Failed to Save Data 1 :" , internetSpeed)
+// })
